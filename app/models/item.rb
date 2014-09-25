87a337042b1bf7eb8835558e5367bfedb352e25d
vagrant <vagrant@rails-dev-box>
2014-09-25 21:51:39 +0000

    model specifications

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ActiveRecord::Base
+  belongs_to :todo
+  belongs_to :grouptodo
+
+  validates :title, length: {minimum: 8}, presence: true
+  validates :todo, presence: true
+  validates :grouptodo, presence: true
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,8 @@
 class Todo < ActiveRecord::Base
+  has_many :items, dependent: :destroy
+
+  belongs_to :user
+
+  validates :title, length: {minimum: 3}, presence: true
+   validates :user, presence: true
 end
