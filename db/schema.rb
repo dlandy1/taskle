8398ea030b8738b14a5668b2f410a5919d31864e
vagrant <vagrant@rails-dev-box>
2014-09-25 21:21:52 +0000

    models

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20140925211804) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.integer  "item_id"
   end
 
   create_table "users", force: true do |t|
