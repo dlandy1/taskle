ae937f19124b55ece88fe6c63834c2f3b6348193
vagrant <vagrant@rails-dev-box>
2014-10-03 18:24:54 +0000

    Add Todo functionality

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,25 +6,40 @@ class TodosController < ApplicationController
   end
 
   def show
-    @topic = Todo.find(params[:todo_id])
-    @items = @todo.items
+      @user = current_user
+      @todo = Todo.find(params[:id])
+      authorize @todo
   end
 
   def edit
+     @todo = Todo.find(params[:id])
+    authorize @todo
+  end
+
+  def new
+    @todo =Todo.new
   end
 
   def create
     @todo = Todo.new(todo_params)
+    @todo.user_id = current_user.id
     if @todo.save
-      redirect_to @todo, notice: "Topic was saved successfully."
+      redirect_to @todo, notice: "Todo was saved successfully."
     else
-      flash[:error] = "Error creating topic. Please try again."
+      flash[:error] = "Error creating todo. Please try again."
       render :new
     end
   end
 
   def update
-    
+    @todo = Todo.find(params[:id])
+    authorize @todo
+    if @todo.update_attributes(todo_params)
+      redirect_to @todo
+    else
+      flash[:error] = "There was an error todo."
+      render :edit
+    end
   end
 
   def destroy
